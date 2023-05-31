7532ab950641501aa6b333e7d37078b126775002
Marcell <marcell.dechant@neuefische.de>
2023-05-31 23:04:58 +0200

    BIG BANG

---
+++ src/main/java/de/neuefische/burgerbestellsystem/BurgerbestellSystemApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.burgerbestellsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerbestellSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerbestellSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/burgerbestellsystem/BurgerbestellSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.burgerbestellsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerbestellSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
